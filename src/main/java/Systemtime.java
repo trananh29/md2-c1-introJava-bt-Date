68000213ccf4c10e33f2490645057261b0e5c09b
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-03-22 16:09:51 +0700

    bt1

---
+++ src/main/java/Systemtime.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+import java.util.Date;
+
+public class Systemtime {
+    public static void main(String[] args){
+        Date now= new Date();
+        System.out.println("New day is: " + now);
+    }
+}
